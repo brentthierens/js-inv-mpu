d2abd5b927b20d7bf71a6d4d678952b8e0cf7edd
Brent Thierens <brent.thierens@gmail.com>
2019-07-17 12:34:12 +0200

    corrected path

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let driver =
     worker: undefined,
     setSampleFreq: function(freq){ this.sampleFreq = freq; },
     setDlpf: function(freq){ this.filterFreq = freq; },
-    measure: function(callback){ new Worker("./worker.js", { workerData: { sample: this.sampleFreq, filter: this.filterFreq }}).on('message', callback); }
+    measure: function(callback){ new Worker(__dirname+"/worker.js", { workerData: { sample: this.sampleFreq, filter: this.filterFreq }}).on('message', callback); }
 };
 
 module.exports = driver;
